c618c83930e9813b8adeca4a07f20961c037418b
4irikon <72358552+4irikon@users.noreply.github.com>
2020-11-05 16:40:33 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -75,8 +75,8 @@ $(document).ready(function(){
 	$('.carousel__inner').slick({
 		speed: 1200,
 		// adaptiveHeight: true,
-		prevArrow: '<button type="button" class="prev"><img src="../icons/left.svg"></button>',
-		nextArrow: '<button type="button" class="next"><img src="../icons/right.svg"></button>',
+		prevArrow: '<button type="button" class="prev"><img src="icons/left.svg"></button>',
+		nextArrow: '<button type="button" class="next"><img src="icons/right.svg"></button>',
 		responsive: [
 			{
 				breakpoint: 992,
